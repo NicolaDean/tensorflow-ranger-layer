e72f176f05eb5f39ef415d587434427a014ee625
zoythum <alessandro.nazzari@polimi.it>
2023-09-07 11:20:33 +0200

    added injection frequencty to FAT

--- fat/single_layer_experiment.py
+++ fat/single_layer_experiment.py
@@ -1,5 +1,6 @@
 from utils.fat_experiment import *
 
+
 '''
 injection_points = ["conv2d", "batch_normalization"]
 injection_points += ["conv2d_"+str(i) for i in range(1, 10)]
@@ -8,11 +9,24 @@ injection_points += ["conv2d_25","conv2d_42","conv2d_56","conv2d_71"]
 injection_points += ["batch_normalization_25", "batch_normalization_42", "batch_normalization_56", "batch_normalization_71"]
 '''
 
-injection_points  = ["conv2d"]
-EXPERIMENT_NAME   = "TEST"
-FINAL_WEIGHT_NAME = "test.h5"
-EPOCHS            = 1
+#injection_points = ["conv2d"]
+#injection_points = ["batch_normalization"]
+#injection_points = ["conv2d_"+str(i) for i in range(1, 10)]
+#injection_points += ["batch_normalization_"+str(i) for i in range(2, 10)]
+injection_points += ["batch_normalization_"+str(i) for i in range(5, 10)]
+injection_points += ["conv2d_25","conv2d_42","conv2d_56","conv2d_71"]
+injection_points += ["batch_normalization_25", "batch_normalization_42", "batch_normalization_56", "batch_normalization_71"]
+
+
+
+LAYER = "batch_normalization_5"
+injection_points  = [LAYER]
+
 
 for layer in injection_points:
+    EXPERIMENT_NAME   = "GOLDEN_SINGLE_LAYER_" + layer
+    FINAL_WEIGHT_NAME = f"single_layer_{layer}_final.h5"
+    EPOCHS            = 36
+
     print(f"Train for layer: {layer}")
-    run_fat_experiment(EPOCHS,EXPERIMENT_NAME,FINAL_WEIGHT_NAME)
+    run_fat_experiment(EPOCHS,EXPERIMENT_NAME,FINAL_WEIGHT_NAME,injection_points = injection_points)
---
+++ fat/utils/callbacks/random_injection.py
@@ -0,0 +1,42 @@
+import tensorflow as tf
+import keras
+import copy
+import sys
+import random
+import numpy as np
+sys.path.append("./../../../model_helper/")
+
+from model_helper.classes_model import *
+
+
+'''
+Activate Classes injection within a certain probability/frequency
+'''
+def layer_activation(self):
+    active = tf.random.uniform([]) < self.extraction_frequency
+    if active:
+            #Disable previously selected injection point:
+            layer = CLASSES_HELPER.get_layer(self.model,self.layer_name,verbose=False)
+            layer.set_mode(ErrorSimulatorMode.enabled)  #Enable the Selected Injection point
+    else:
+            #Disable previously selected injection point:
+            layer = CLASSES_HELPER.get_layer(self.model,self.layer_name,verbose=False)
+            layer.set_mode(ErrorSimulatorMode.disabled)  #Enable the Selected Injection point
+
+class ClassesSingleLayerInjection(keras.callbacks.Callback):
+
+    def __init__(self, CLASSES,layer_name,extraction_frequency = 1.0, use_batch = False):
+        super().__init__()
+        self.injection_points     = CLASSES.get_injection_points()
+        self.CLASSES              = CLASSES
+        self.extraction_frequency = extraction_frequency
+        self.use_batch            = use_batch
+        self.layer_name           = layer_name
+
+    def on_train_batch_begin(self, epoch, logs=None):
+        if self.use_batch:
+            layer_activation(self)
+
+    def on_epoch_begin(self, epoch, logs=None):
+        if not self.use_batch:
+            layer_activation(self)
